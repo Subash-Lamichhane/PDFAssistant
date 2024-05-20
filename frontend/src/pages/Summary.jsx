889b8b2684e7604402271a59581af01f6f536e6e
Subash-Lamichhane <077bct081.subash@pcampus.edu.np>
2024-05-20 12:50:53 +0545

    frontend:loading added when question is asked

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -5,16 +5,16 @@ import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
 const Summary = () => {
-    const qnanswer = {
-        "result": {
-            "answer": [
-                "yes"
-            ],
-            "explanation": [
-                "Solar flares are magnetic eruptions from the Suns corona which can cause severe disruptions to satellite communication and electrical grids."
-            ]
-        }
-    }
+    // const qnanswer = {
+    //     "result": {
+    //         "answer": [
+    //             "yes"
+    //         ],
+    //         "explanation": [
+    //             "Solar flares are magnetic eruptions from the Suns corona which can cause severe disruptions to satellite communication and electrical grids."
+    //         ]
+    //     }
+    // }
     const location = useLocation();
     const summaryData = location.state?.response || [];
     const pdffile = location.state?.selectedFile;
@@ -23,17 +23,17 @@ const Summary = () => {
     // console.log("Tag data=",JSON.parse(tagData))
     const [question, setQuestion] = useState('');
     const [qnans, setQnans] = useState([]);
-
+    const [loading, setLoading] = useState(false)
     const capitalizeTitle = (title) => {
         if (typeof title !== 'string') {
             return ''; // Or handle the case where title is not a string as needed
         }
         return title
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
+            .split(' ')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
     };
-    
+
     const title = capitalizeTitle(titleData)
 
     const askAIHandler = async () => {
@@ -43,16 +43,16 @@ const Summary = () => {
             formDataSummary.append('function_name', 'get_answer');
             formDataSummary.append('question', question);
             formDataSummary.append('page_no', 0);
-
+            setLoading(true)
             const response = await axios.post('http://127.0.0.1:8000/process_pdf/', formDataSummary, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
             });
-
+            setLoading(false)
             // console.log(response);
             const qnan = [question, response.data.result.explanation]
-            setQnans([...qnans, qnan]);
+            setQnans([qnan, ...qnans]);
             setQuestion('');
         }
     }
@@ -101,6 +101,34 @@ const Summary = () => {
                     </div>
                     <div>
                         <h2 className="text-3xl font-bold tracking-tighter ">Answer To Questions Asked (AI)</h2>
+                        {loading == true && (
+                            <div className="flex flex-1 items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
+                                <button
+                                    disabled
+                                    type="button"
+                                    className="py-4 px-8 text-lg font-medium text-gray-900 bg-white rounded-lg border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 inline-flex items-center"
+                                >
+                                    <svg
+                                        aria-hidden="true"
+                                        role="status"
+                                        className="inline w-6 h-6 mr-4 text-gray-200 animate-spin"
+                                        viewBox="0 0 100 101"
+                                        fill="none"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                            fill="currentColor"
+                                        />
+                                        <path
+                                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                            fill="#1C64F2"
+                                        />
+                                    </svg>
+                                    Generating answers...
+                                </button>
+                            </div>
+                        )}
                         <div className="mt-4 space-y-4">
                             {qnans.map((q, index) => (
                                 <div key={index} className="rounded-md border border-gray-200 bg-white p-4 shadow-sm">
